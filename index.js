282188f23a369fa77c9a2debafb70db78498ecb7
rishikeshmore5 <127827408+rishikeshmore5@users.noreply.github.com>
2023-08-26 23:26:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
 
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
+//app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL, {
